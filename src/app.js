355a198caf0421118d5d71ac67172c942eccef07
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2024-12-28 13:44:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials: true   
-}))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+//routes import
+import userRouter from "./routes/user.routes.js";
 
+//routes declaration
 
-export {app}
+app.use("/api/v1/users",userRouter)
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -5,16 +6,15 @@ dotenv.config({
   path: "./env",
 });
 
-
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at port: ${process.env.PORT}`)
-    } )
-})
-.catch((err)=>{
-    console.log(`MongoDB Connection Failed!! ${err} `)
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB Connection Failed!! ${err} `);
+  });
 
 // (async () => {
 //     try {
